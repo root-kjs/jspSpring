df513731f14d77d2956d9bd96132651301973356
itdanjalog <kgs2072@naver.com>
2025-07-31 10:28:09 +0900

    day02

---
+++ src/main/java/example/day02/AppStart.java
@@ -0,0 +1,14 @@
+package example.day02;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppStart {
+
+    public static void main(String[] args) {
+        SpringApplication.run( AppStart.class );
+    }
+
+} // class end
