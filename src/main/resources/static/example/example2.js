08d948c1d35ac7872f167250fee9288227c4aac7
itdanjalog <kgs2072@naver.com>
2025-08-04 13:08:10 +0900

    day04

--- src/main/resources/static/example/example2.js
+++ src/main/resources/static/example/example2.js
@@ -22,7 +22,7 @@ const func3 = ( ) => { console.log( 'func3 exe');
 // (4)
 const func4 = ( ) => { console.log( 'func4 exe');
     fetch( "/day04/exam4" , { method : "DELETE"} );
-}
+} // func end 
 
 // (5)
 const func5 = ( ) => { console.log( 'func5 exe');
@@ -35,16 +35,60 @@ const func5 = ( ) => { console.log( 'func5 exe');
         .catch( error => { console.log(error) } )   // 3) 만약에 fetch 통신간에 오류 발생시 실행문처리
 } // func end 
 
+// (6)
+const func6 = ( )=>{ console.log( 'func6 exe');
+    const name = "유재석";  const age = 10;
+    // + fetch 함수를 이용한 BODY본문 방식 , 
+    const option = { 
+        method  : "POST" , 
+        headers : { "Content-Type" : "application/json" } ,  // HTTP BODY 타입 설정 
+        body : JSON.stringify( { name : name , age : age } )  // JSON 타입으로 변환 , 
+        // JSON.stringify( ) : JS객체 --> 문자열 타입 변환 
+        // JSON.parse( ) : 문자열 타입 --> JS객체 변환 
+    } // option end 
+    fetch( "/day04/exam6" , option )                // 1] URL   2] option { method : , headers : , body :}
+        .then( response => response.json() )        // 3] 응답 자료 타입 변환 , JSON 
+        .then( data => { console.log( data ) } )    // 4] 타입변환된 자료 , data 
+        .catch( error => { console.log( error ) } ) // 5] 예외 발생시 , error 
+} // func end 
+
+// (7) PUT 
+const func7 = ( ) => { console.log( 'func7 exe');
+    let sample = { name : "유재석" , age : 10 };
+    let option = {
+        method : "PUT",
+        headers : { "Content-Type" : "application/json"} ,
+        body : JSON.stringify( sample ),
+    } //option end
+    fetch( "/day04/exam7" , option ) 
+        .then( response => response.json() ) // + then/catch 함수내 매개변수명은 임의로 한다.
+        .then( data => console.log(data) )
+        .catch( error => { console.log( error ) } )
+} // func end 
+
+// (8) DELETE 
+const func8 = ( ) => { console.log( 'func8 exe');
+    let name = "유재석"; let age = 40;
+    // + option 
+    fetch( `/day04/exam8?name=${name}&age=${age}` , { method : "DELETE"} )
+        .then( response => response.json() )
+        .then( data => { console.log(data); } )
+        .catch( error => { console.log( error); } )
+} // func end 
+
 /*
     [FECTH] 라이브러리 (내장)함수
-        - 외부 주소로 부터 요청 과 응답을 *비동기 통신* 지원하는 함수 
+        - 외부 주소로 부터 요청 과 응답을 *비동기 통신* 지원하는 함수 , * 비동기통신 : fetch(JS내장), ajax(JQUERY), axios(React)
         - 사용법 
-            fetch( URL, HEADER ).then( response => response.json() ).then( data => {  } ).catch
+            fetch( URL, option ).then( response => response.json() ).then( data => {  } ).catch
                 1) 통신할 URL 주소 
-                2) 통신할 HEADER 옵션 
-                    { method : "HTTP메소드"}
+                2) 통신할 option : 주로 POST , PUT 
+                    { 
+                        method : "POST or PUT" , 
+                        headers : { "Content-Type" : "application/json"},
+                        body : JSON.stringify( { } ) 
+                    }
                 3) then( response => response.json() ) : 응답 자료를 매개변수로 받아 JSON 타입으로 변환 
                 4) then( data => { 실행문; } )          : 타입변환된 자료를 매개변수로 받아 실행문처리
                 5) catch( error => { 실행문; } )        : 만약에 fetch 통신간에 오류 발생시 실행문 처리 
-
 */
