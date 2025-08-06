cc896c8a26c4c964482c0f6f7183ebef78d7d62c
itdanjalog <kgs2072@naver.com>
2025-08-06 10:22:57 +0900

    day06

---
+++ src/main/resources/static/example/example3.js
@@ -0,0 +1,19 @@
+console.log("example3.js open");
+
+// (1) 비동기 fetch
+// 람다식함수 : const func1 = ( ) => { }
+// 선언적함수 : function func1( ){ }
+const func1 = ( ) => {
+    console.log( '[1] fetch 전 ')
+    //(fetch) fetch가 실행되고 결과(response)가 오기전에 다음코드로 이동 , 즉] JS는 *응답* 기다리지 않는다.
+    const option = { method : "GET" }
+    fetch( "/day06/exam1" , option )
+        .then( response => response.json() )
+        .then( data => {
+            console.log( data );
+            console.log( '[2] fetch 통신 결과')
+        })
+        .catch( error => { console.log( error )})
+    console.log( '[3] fetch 후 ')
+    // 코드 실행 예측 : [1] -> [2] -> [3] , 결과 : [1] -> [3] -> [2]
+}
