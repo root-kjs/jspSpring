ea574155ebdfa6780bbad4784fe5b783d9562c91
itdanjalog <kgs2072@naver.com>
2025-08-25 13:34:24 +0900

    day63

--- src/main/java/web/model/dao/PostDao.java
+++ src/main/java/web/model/dao/PostDao.java
@@ -6,6 +6,8 @@ import web.model.dto.PostDto;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class PostDao extends Dao {
@@ -27,4 +29,53 @@ public class PostDao extends Dao {
         } catch (Exception e) {  System.out.println(e); }
         return 0; // 등록 실패시
     } // func end
+
+    // [2-1] 카테고리별 게시물 수
+    public int getTotalCount( int cno  ){
+        try{ String sql = "select count(*) from post where cno = ? "; // count(*) : 레코드 전체수를 반환하는 함수
+            PreparedStatement ps = conn.prepareStatement( sql );
+            ps.setInt( 1 , cno );
+            ResultSet rs = ps.executeQuery();
+            if( rs.next() ){return rs.getInt( 1 ); }// 첫번째 레코드의 속성값 1개 반환
+        } catch (Exception e) {     System.out.println(e);     }
+        return 0; // 조회 결과 없으면 0 반환
+    } // func end
+
+    // [2-2] 카테고리별 전체 게시물 정보 조회
+    public List<PostDto> findAll( int cno , int startRow , int count ){
+        List<PostDto> list = new ArrayList<>();
+        try{ String sql = "select * from post where cno = ? order by pno desc limit ? , ? ";
+            PreparedStatement ps = conn.prepareStatement( sql );
+            ps.setInt( 1 , cno );
+            ps.setInt( 2 , startRow );
+            ps.setInt( 3 , count );
+            ResultSet rs = ps.executeQuery();
+            while ( rs.next() ){
+                PostDto postDto = new PostDto();
+                postDto.setMno( rs.getInt("mno") );
+                postDto.setCno( rs.getInt("cno") );
+                postDto.setPcontent( rs.getString("pcontent"));
+                postDto.setPdate( rs.getString("pdate") );
+                postDto.setPview( rs.getInt("pview") );
+                postDto.setPno( rs.getInt( "pno") );
+                list.add( postDto );
+            }
+        } catch (Exception e) {  System.out.println(e);}
+        return list;
+    } // func end
+
+
+
 } // class end
+
+
+
+
+
+
+
+
+
+
+
+
